574534e53371b7769951f81cf3d05ad64702d129
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 12:08:39 -0300

    operadores de atribuição

---
+++ operadores/operadores_de_atribuicao.py
@@ -0,0 +1,32 @@
+# Reforçando operdores de atribuição
+
+#Atribuição simples
+saldo = 500 
+
+#Atribuição com adição
+saldo += 200
+
+print(saldo)
+
+#Atribuição com subtração, divisão, Multiplicação, Modulo
+saldo -= 100
+print(saldo)
+
+saldo *= 2
+print(saldo)
+
+saldo /= 5
+print(saldo)
+
+saldo //= 5
+print(saldo)
+
+saldo = 500
+print(saldo)
+
+saldo %= 480
+print(saldo)
+
+#Atribuição com exponenciação
+saldo **= 2
+print(saldo)
