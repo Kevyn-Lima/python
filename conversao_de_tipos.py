fbd948f47f80bb3e957bac93a8353a1b928c9b1f
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 11:23:14 -0300

    Conversão de Tipos em Python

---
+++ conversao_de_tipos.py
@@ -0,0 +1,16 @@
+# preco = 10
+# preco = float(preco)
+
+# idade = 28
+
+# print(str(preco))
+
+# print(str(idade))
+
+# texto = f"Idade {idade}, preco {preco}"
+# print(type(texto))
+
+# preco = "10.50"
+# idade = "28"
+
+# print(f"O preco esta {float(preco)}, mas eu so tenho {int(idade)} anos")
