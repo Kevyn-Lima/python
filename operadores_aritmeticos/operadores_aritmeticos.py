1be23a3502b80c94072490dad26295f64bac0227
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 11:57:44 -0300

    Operadores Aritiméticos

---
+++ operadores_aritmeticos/operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+#Apenas reforçando operadores Aritméticos em Python
+
+#Adição
+print(1+1)
+
+#Subtração
+print(2-1)
+
+#multiplicação
+print(2*3)
+
+#divisão
+print(12/3)
+
+#divisão por inteiro
+print(12//2)
+
+#Módulo
+print(10 % 3)
+
+#Exponenciação
+print(2 ** 3)
