6cc6b5f6f98f431b4ba69fe391f71a5784152cdc
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 12:28:03 -0300

    Operadores de associação para validação em sequencia

---
+++ operadores/operadores_de_associacao.py
@@ -0,0 +1,11 @@
+#Reforçando operdadores de associação.
+
+curso = "Curso de Python"
+frutas = ["Laranja", "Uva", "Limão"]
+saques = [1500, 100]
+
+print("Python" in curso)
+
+print("maçã" not in frutas)
+
+print(200 in saques)
