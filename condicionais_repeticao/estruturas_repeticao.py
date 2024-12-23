396e7483885b103b082404d9394a6fd8e2efc224
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 15:45:13 -0300

    Validando um break, igual switch case em JS

--- condicionais_repeticao/estruturas_repeticao.py
+++ condicionais_repeticao/estruturas_repeticao.py
@@ -12,32 +12,42 @@
 #range(stop) -> range object
 #range(start, stop[, step]) -> range object
 
-import sys
+# import sys
 
 
-print(list(range(4)))
+# print(list(range(4)))
 
-#Utilizando range com for
-for numero in range(0, 11):
-    print(numero, end=" ")
+# #Utilizando range com for
+# for numero in range(0, 11):
+#     print(numero, end=" ")
 
-print(" ")
+# print(" ")
 
-#Exibindo a tabuada do 5
+# #Exibindo a tabuada do 5
 
-for numero in range(0, 51, 5):
-    print(numero, end=" ")
+# for numero in range(0, 51, 5):
+#     print(numero, end=" ")
 
-print(" ")
-#Comando While em python
+# print(" ")
+# #Comando While em python
+
+# opcao = -1
+# while opcao !=0:
+#     opcao = int(input("[1] Sacar\n[2] Extrato \n[0] Sair \n: "))
+
+#     if opcao == 1:
+#         print("Sacando...")
+#     elif opcao == 2:
+#         print("Exibindo...")
+# else:
+#     print("Obrigado por usar nosso sistema bancário, até logo.")
+
 
 opcao = -1
-while opcao !=0:
-    opcao = int(input("[1] Sacar\n[2] Extrato \n[0] Sair \n: "))
+while opcao != 0:
+    opcao = int(input("Informe um número: "))
 
-    if opcao == 1:
-        print("Sacando...")
-    elif opcao == 2:
-        print("Exibindo...")
-else:
-    print("Obrigado por usar nosso sistema bancário, até logo.")
+    if opcao == 10:
+        break
+    print(opcao)
+#Apenas validando um break como em switch case em JS.
