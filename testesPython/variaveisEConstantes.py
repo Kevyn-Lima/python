5be7c69bcd581459dd234cd566697853d9f05d6b
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 11:11:30 -0300

    Subindo informações basicas de Python

---
+++ testesPython/variaveisEConstantes.py
@@ -0,0 +1,13 @@
+# age = 23
+# name = 'Kevyn'
+# PI = 3.14
+# age, name = (25, 'Kevyn Lima')
+# print(f'Eu me chamo {name} e tenho {age} anos de idade. E sei que PI e {PI}')
+
+# Em python, utilizamos para declaração de variaveis o snake case
+# limite_saque_diario
+
+# BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
