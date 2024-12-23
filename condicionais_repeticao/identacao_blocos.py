50a968176caa82d47db6daf8ca53c7adb94556bb
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-23 14:08:47 -0300

    Identação e Blocos

---
+++ condicionais_repeticao/identacao_blocos.py
@@ -0,0 +1,9 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor Sacado!")
+sacar(250)
+
+#Reforçando que sua identação é a forma de identificação dos blocos, para que 
+# não tenha nenhum problema, e se torne mais facil a manutenção.
